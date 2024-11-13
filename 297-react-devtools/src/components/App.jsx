aa8f62448235f3e0c8caf5112a2934e9b260c949
Yashwanth A L <yashwanthal2004@gmail.com>
2024-11-13 19:52:44 +0530

    added react devtools

---
+++ 297-react-devtools/src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "./Card";
+import Avatar from "./Avatar";
+import contacts from "../contacts";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar src="https://randomuser.me/api/portraits/men/1.jpg"/>
+      {contacts.map(contact => (
+        <Card
+          name={contact.name}
+          img={contact.imgURL}
+          tel={contact.phone}
+          email={contact.email}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
