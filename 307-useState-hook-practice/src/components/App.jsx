a139027743c083aff0f31d35e8ee4ac8653c3b8c
Yashwanth A L <yashwanthal2004@gmail.com>
2024-11-13 20:36:39 +0530

    added use State hook practice model

--- 307-useState-hook-practice/src/components/App.jsx
+++ 307-useState-hook-practice/src/components/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
 function App() {
+
+  const now = new Date().toLocaleTimeString();
+  const [time, setTime] = React.useState(now);
+  setInterval(updateTime, 1000);
+
+  function updateTime(){
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
