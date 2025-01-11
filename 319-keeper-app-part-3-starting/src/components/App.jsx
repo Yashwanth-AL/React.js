65f594543647f093a9e9ba8b519f6de426c4d587
Yashwanth A L <yashwanthal2004@gmail.com>
2025-01-12 00:31:10 +0530

    added keeper notes app part 3

--- 319-keeper-app-part-3-starting/src/components/App.jsx
+++ 319-keeper-app-part-3-starting/src/components/App.jsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes(prev => {
+      return [...prev, note];
+    })
+  } 
+  
+  function deleteNote(id) {
+    setNotes(prev => {
+      return prev.filter((item, index) => index !== id);
+    })
+  }
+  
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={ addNote } />
+      { notes.map((note, index) => {
+        return (
+          <Note 
+            key={ index }
+            id={ index }
+            title={ note.title } 
+            content={ note.content } 
+            onDelete={ deleteNote }
+          />)
+      }) }
       <Footer />
     </div>
   );
