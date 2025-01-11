2bb1cc6bf7affbd34cd18086ffa7780dd274e964
Yashwanth A L <yashwanthal2004@gmail.com>
2025-01-11 13:18:31 +0530

    updated changing complex states

--- 313-changing-complex-state/src/components/App.jsx
+++ 313-changing-complex-state/src/components/App.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+
+  const [fullName, setFNname] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function updateFullName(e) {
+    const {name, value} = e.target;
+
+    setFNname(prev => {
+      if(name === "fName") {
+        return {
+          fName : value,
+          lName : prev.lName
+        };
+      } else if(name === "lName") {
+        return {
+          fName : prev.fName,
+          lName : value
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fullName.fName} {fullName.lName} </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input 
+          name="fName" 
+          onChange={updateFullName} 
+          placeholder="First Name" 
+          value={fullName.fName}
+        />
+        <input 
+          name="lName" 
+          onChange={updateFullName} 
+          placeholder="Last Name" 
+          value={fullName.lName}  
+        />
         <button>Submit</button>
       </form>
     </div>
